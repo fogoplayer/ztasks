0c8d128305c827b1a7f6d9b604671c067d667192
Zarin Loosli <fogoplayer@gmail.com>
2018-09-14 12:50:49 -0600

    Re-modularized code

--- js/List.js
+++ js/List.js
@@ -1,73 +1,63 @@
-import Task from "./Task.js";
-
 class List {
-    constructor(){
-        this.tasks = [{
-                name: "task1",
-                checked: false,
-                dueDate:new Date("July 2"),
-                notes: "Blah blah blah",
-                subtasks: [{
-                        name: "subtask3",
-                        subtasks: [{
-                                name: "subtask1",
-                                subtasks: []
-                            },
-                            {
-                                name: "subtask2",
-                                checked: true,
-                                subtasks: []
-                            }
-                        ]
-                    },
-                    {
-                        name: "subtask0",
-                        checked: true,
-                        subtasks: []
-                    }
-                ]
-            },
-            {
-                name: "task2",
-                checked: false,
-                randomProp: "asdf",
-                subtasks: []
-            }
-        ];
-        this.dismissedTasks=[];
-        
-        //Initial render
-        if (window.location.pathname === "/") {
-            this.renderTasks();
-            document.getElementById("newTask").onclick = () => {
-                this.addTask();
-            };
-        }
-        
-    }
-    
     /**
      * Renders all tasks at a certain level in the list
      * @param id-the id of the collapsible-body where the tasks will be placed
      * @return null
     **/
-    renderTasks(id = "root") {
+    static renderTasks(id = "root") {
         document.getElementById("body_" + id).innerHTML="";
         if (id === "root" || id === "") {
-            this.tasks.forEach(task => {
-                this.createTaskNode(task, [this.tasks.findIndex(t => t.name === task.name)]);
+            tasks.forEach(task => {
+                List.createTaskNode(task, [tasks.findIndex(t => t.name === task.name)]);
             });
         }
         else {
-            let tasksArray = this.tasks;
+            let tasksArray = tasks;
             const reference = id.split("_").map(Number);
             reference.forEach(ref => {
                 tasksArray = tasksArray[ref].subtasks;
             });
             tasksArray.forEach(task => {
-                this.createTaskNode(task, reference.concat([tasksArray.findIndex(t => t.name === task.name)]), id);
+                List.createTaskNode(task, reference.concat([tasksArray.findIndex(t => t.name === task.name)]), id);
             });
         }
+    }
+    
+    /**
+     * @param id-the id of the parent task
+     * @return parent.subtasks
+    **/
+    static getSubtasksFromId(id){
+        let tasksArray = tasks;
+        if (id !== "" && id !== "root") {
+            const reference = id.split("_").map(Number);
+            reference.forEach(ref => {
+                tasksArray = tasksArray[ref].subtasks;
+            });
+        }
+        return tasksArray;
+    }
+    
+    /**
+     * Convert ID to task object
+     * @param id-the id of the parent task
+     * @return task object
+    **/
+    static getTaskFromId(id){
+        let tasksArray = tasks;
+        let task;
+        if (id !== "" && id !== "root") {
+            const reference = id.split("_").map(Number);
+            reference.forEach(ref => {
+                task = tasksArray[ref];
+                tasksArray = task.subtasks;
+            });
+        }else{
+            task = {
+                subtasks:tasks
+            };
+        }
+        return task;
     }
     
     /**
@@ -77,8 +67,8 @@ class List {
      * @param parent-the id of the parent node where the task is being inserted
      * @return-either null, or the task node
     **/
-    createTaskNode(taskObject, reference, parent = "root") {
-
+    static createTaskNode(taskObject, reference, parent = "root") {
+    
         //Add chevron to parent if not root and parent doesn't already have one
         if (parent !== "root" && !document.getElementById("header_" + parent).querySelector(".chevron")) {
             const chevronContainer = document.createElement("div");
@@ -139,7 +129,7 @@ class List {
                     taskName.contentEditable=true;
                     if(taskObject.name){ taskName.innerText = taskObject.name }
                     taskName.onkeyup= (e) => {
-                        this.tasks = Task.deleteOrSaveOnKeyup(document.getElementById("header_" + id).querySelector(".taskName").innerHTML, id, this);
+                        tasks = Task.deleteOrSaveOnKeyup(document.getElementById("header_" + id).querySelector(".taskName").innerHTML, id);
                     };
                     taskName.onkeydown = (keypress) => {
                         switch(keypress.key){
@@ -150,7 +140,7 @@ class List {
                                     location.pathname = "/task-details#" + id;
                                 }else
                                 {
-                                    this.addTask(parent);
+                                    Task.addTask(parent);
                                 }
                                 break;
                                 
@@ -159,19 +149,19 @@ class List {
                                 break;
                                 
                             case "ArrowLeft":
-                                if(keypress.ctrlKey){ this.moveTask("left",id); }
+                                if(keypress.ctrlKey){ Task.moveTask("left",id); }
                                 break;
                                 
                             case "ArrowRight":
-                                if(keypress.ctrlKey){ this.moveTask("right",id); }
+                                if(keypress.ctrlKey){ Task.moveTask("right",id); }
                                 break;
                                 
                             case "ArrowUp":
-                                if(keypress.ctrlKey){ this.moveTask("up",id); }
+                                if(keypress.ctrlKey){ Task.moveTask("up",id); }
                                 break;
                             
                             case "ArrowDown":
-                                if(keypress.ctrlKey){ this.moveTask("down",id); }
+                                if(keypress.ctrlKey){ Task.moveTask("down",id); }
                                 break;
                                 
                             default:
@@ -237,39 +227,39 @@ class List {
             const menu = document.createElement("ul");
                 menu.id = "menu" + id;
                 menu.classList.add("dropdown-content");
-                const openTaskDetails = document.createElement("li");
+                const openTaskDetailsContainer = document.createElement("li");
                     const openTaskDetailsLink = document.createElement("a");
                         openTaskDetailsLink.innerHTML = "Open Task Details";
                         openTaskDetailsLink.href="/task-details#"+id;
-                        openTaskDetails.appendChild(openTaskDetailsLink);
-                    menu.appendChild(openTaskDetails);
-                const addSubtask = document.createElement("li");
+                        openTaskDetailsContainer.appendChild(openTaskDetailsLink);
+                    menu.appendChild(openTaskDetailsContainer);
+                const addSubtaskContainer = document.createElement("li");
                     const addSubtaskLink = document.createElement("a");
                         addSubtaskLink.innerHTML = "Add Subtask";
-                        addSubtaskLink.onclick = () => { this.addTask(id) };
-                        addSubtask.appendChild(addSubtaskLink);
-                    menu.appendChild(addSubtask);
-                const deleteTask = document.createElement("li");
+                        addSubtaskLink.onclick = () => { Task.addTask(id) };
+                        addSubtaskContainer.appendChild(addSubtaskLink);
+                    menu.appendChild(addSubtaskContainer);
+                const deleteTaskContainer = document.createElement("li");
                     const deleteTaskLink = document.createElement("a");
                         deleteTaskLink.innerHTML = "Delete Task";
-                        deleteTaskLink.onclick = () => {Task.deleteTask(id, this);};
-                        deleteTask.appendChild(deleteTaskLink);
-                    menu.appendChild(deleteTask);
+                        deleteTaskLink.onclick = () => {Task.deleteTask(id);};
+                        deleteTaskContainer.appendChild(deleteTaskLink);
+                    menu.appendChild(deleteTaskContainer);
                 li.appendChild(menu);
                 
             //Set up drag/drop
             li.draggable = "true";
-            li.ondragstart = (e) => { e.stopPropagation(); setTimeout(() => { li.remove(); }, 1); collapsibleHeader.style.paddingLeft = "0"; e.dataTransfer.setDragImage(li, 0, 15); this.taskBeingDragged = taskObject; let parentArray = (parent === "root") ? this.tasks : this.getTasksArrayFromId(parent); parentArray.splice(parentArray.findIndex(t => t.name === taskObject.name), 1); };
+            li.ondragstart = (e) => { e.stopPropagation(); setTimeout(() => { li.remove(); }, 1); collapsibleHeader.style.paddingLeft = "0"; e.dataTransfer.setDragImage(li, 0, 15); taskBeingDragged = taskObject; let parentArray = (parent === "root") ? tasks : List.getSubtasksFromId(parent); parentArray.splice(parentArray.findIndex(t => t.name === taskObject.name), 1); };
             li.ondragover = (e) => { e.stopPropagation(); li.style.marginBottom="38px"; return false };
             li.ondragleave = (e) => { e.stopPropagation(); li.style.marginBottom=0 };
-            li.ondrop = (e) =>{ e.stopPropagation(); const mouseDepth = Math.round((e.clientX - 8)/ 30 ); let tempRef = reference.slice(0,mouseDepth); const tasksArray = this.getTasksArrayFromId(tempRef.join("_")); tasksArray.push(this.taskBeingDragged); this.renderTasks(); };
+            li.ondrop = (e) =>{ e.stopPropagation(); const mouseDepth = Math.round((e.clientX - 8)/ 30 ); let tempRef = reference.slice(0,mouseDepth); const tasksArray = List.getSubtasksFromId(tempRef.join("_")); tasksArray.push(taskBeingDragged); List.renderTasks(); };
             document.getElementById("body_" + parent).appendChild(li);
             
             //Add subtasks, if they exist
             if(taskObject.subtasks && taskObject.subtasks.length){
                 li.classList.add("active");
                 taskObject.subtasks.forEach(subtask => {
-                    this.createTaskNode(subtask, reference.concat([taskObject.subtasks.findIndex(t => t.name === subtask.name)]), id);
+                    List.createTaskNode(subtask, reference.concat([taskObject.subtasks.findIndex(t => t.name === subtask.name)]), id);
                 });
             }
             //Init Materialize
@@ -281,126 +271,6 @@ class List {
             M.Collapsible.init(document.querySelectorAll('.collapsible'), {
                 accordion: false
             });
-    }
-    
-    /**
-     * Add task to database and update UI
-     * @param id-the id of the parent, as an underscore-separated string
-     * @return null
-    **/
-    addTask(id = "root"){
-        const newTask = {
-                name: "",
-                checked: false,
-                dueDate: null,
-                notes: "",
-                subtasks: []
-            };
-        let tasksArray;
-        let reference;
-        if(id === "root"){
-            tasksArray = this.tasks;
-            reference = [];
-        }else{
-            tasksArray = this.tasks;
-            reference = id.split("_").map(Number);
-            reference.forEach(ref => {
-                tasksArray = tasksArray[ref].subtasks;
-            });
-            
-        }
-        tasksArray.push(newTask);
-        this.createTaskNode(newTask, reference.concat([tasksArray.length - 1]), id);
-        document.getElementById("header_"+reference.join("_")+ (id !== "root" ? "_" : "") +(tasksArray.length - 1)).querySelector(".taskName").focus();
-    }
-    
-    /**
-     * Intent a task one way or the other
-     * @param direction-string, either "left" or "right"
-     * @param id-the id of the task being indented
-     * @return null
-    **/
-    moveTask(direction, id){
-        //Set task objects as variables
-        const twoLevelsUp = this.getTaskFromId(id.substring(0, id.length - 4));
-        const oneLevelUp = this.getTaskFromId(id.substring(0, id.length - 2));
-        const taskObject = this.getTaskFromId(id);
-        const prevSibling = oneLevelUp.subtasks[oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name) - 1];
-        const nextSibling = oneLevelUp.subtasks[oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name) + 1];
-        this.taskBeingDragged=true;
-        if(direction === "left" && oneLevelUp.name){
-            const youngerSiblings = oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name) + 1);
-            taskObject.subtasks = taskObject.subtasks.concat(youngerSiblings);
-            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.length - 1, 1);
-            twoLevelsUp.subtasks.splice(twoLevelsUp.subtasks.findIndex(t => t.name === oneLevelUp.name) + 1, 0, taskObject);
-            this.renderTasks(twoLevelsUp.name ? id.substring(0, id.length - 4) : "root");
-            document.getElementById("header_" + (id.length > 3 ? id.substring(0, id.length - 4) + "_" : "") + (Number(id.substring(id.length - 3, id.length - 2)) + 1)).children[1].focus();
-        }else if(direction === "right" && prevSibling){
-            prevSibling.subtasks.push(taskObject);
-            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name), 1);
-            this.renderTasks(oneLevelUp.name ? id.substring(0, id.length - 2) : "root");
-            document.getElementById( "header_" + (id.length > 1 ? id.substring(0, id.length - 2) + "_" : "") + (prevSibling ? (oneLevelUp.subtasks.findIndex(t => t.name === prevSibling.name)) + "_" : "") + (prevSibling.subtasks.length - 1) ).children[1].focus();
-        }else if(direction === "up" && prevSibling){
-            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name), 1);
-            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === prevSibling.name), 0, taskObject);
-            this.renderTasks(oneLevelUp.name ? id.substring(0, id.length - 2) : "root");
-            document.getElementById("header_" + (id.length > 1 ? id.substring(0, id.length - 2) + "_" : "") + (prevSibling ? oneLevelUp.subtasks.findIndex(t => t.name === prevSibling.name) - 1: "") ).children[1].focus();
-        }else if(direction === "down" && nextSibling){
-            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name), 1);
-            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === nextSibling.name) + 1, 0, taskObject);
-            this.renderTasks(oneLevelUp.name ? id.substring(0, id.length - 2) : "root");
-            document.getElementById("header_" + (id.length > 1 ? id.substring(0, id.length - 2) + "_" : "") + (nextSibling ? oneLevelUp.subtasks.findIndex(t => t.name === nextSibling.name) + 1: "")).children[1].focus();
-        }
-        this.taskBeingDragged = false;
-    }
-    
-    /**
-     * Dismiss a task
-     * @param id-The id of the task being dismissed
-     * return null
-    **/
-    dismissTask(id){
-        const oneLevelUp = this.getTaskFromId(id.substring(0, id.length - 2));
-        const taskObject = this.getTaskFromId(id);
-        oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name), 1);
-        this.dismissedTasks.push(taskObject);
-        this.renderTasks(id.substring(0, id.length - 2));
-    }
-    
-    /**
-     * @param id-the id of the parent task
-     * @return parent.subtasks
-    **/
-    getTasksArrayFromId(id){
-        let tasksArray = this.tasks;
-        if (id !== "" && id !== "root") {
-            const reference = id.split("_").map(Number);
-            reference.forEach(ref => {
-                tasksArray = tasksArray[ref].subtasks;
-            });
-        }
-        return tasksArray;
-    }
-    
-    /**
-     * Convert ID to task object
-     * @param id-the id of the parent task
-     * @return task object
-    **/
-    getTaskFromId(id){
-        let tasksArray = this.tasks;
-        let task;
-        if (id !== "" && id !== "root") {
-            const reference = id.split("_").map(Number);
-            reference.forEach(ref => {
-                task = tasksArray[ref];
-                tasksArray = task.subtasks;
-            });
-        }else{
-            task = {};
-            task.subtasks = this.tasks;
-        }
-        return task;
     }
 }
 
--- js/Task.js
+++ js/Task.js
@@ -1,38 +1,116 @@
 class Task {
-    constructor() {}
-
     /**
-     * When a task unfocuses, save it, or delete it if it's now blank
+     * Add task to database and update UI
+     * @param id-the id of the parent, as an underscore-separated string
+     * @return null
+    **/
+    static addTask(id = "root"){
+        const newTask = {
+                name: "",
+                checked: false,
+                dueDate: null,
+                notes: "",
+                subtasks: []
+            };
+        let tasksArray;
+        let reference;
+        if(id === "root"){
+            tasksArray = tasks;
+            reference = [];
+        }else{
+            tasksArray = tasks;
+            reference = id.split("_").map(Number);
+            reference.forEach(ref => {
+                tasksArray = tasksArray[ref].subtasks;
+            });
+            
+        }
+        tasksArray.push(newTask);
+        List.createTaskNode(newTask, reference.concat([tasksArray.length - 1]), id);
+        document.getElementById("header_"+reference.join("_")+ (id !== "root" ? "_" : "") +(tasksArray.length - 1)).querySelector(".taskName").focus();
+    }
+    
+    /**
+     * Intent a task one way or the other
+     * @param direction-string, either "left" or "right"
+     * @param id-the id of the task being indented
+     * @return null
+    **/
+    static moveTask(direction, id){
+        //Set task objects as variables
+        const twoLevelsUp = List.getTaskFromId(id.substring(0, id.length - 4));
+        const oneLevelUp = List.getTaskFromId(id.substring(0, id.length - 2));
+        const taskObject = List.getTaskFromId(id);
+        const prevSibling = oneLevelUp.subtasks[oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name) - 1];
+        const nextSibling = oneLevelUp.subtasks[oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name) + 1];
+        taskBeingDragged=true;
+        if(direction === "left" && oneLevelUp.name){
+            const youngerSiblings = oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name) + 1);
+            taskObject.subtasks = taskObject.subtasks.concat(youngerSiblings);
+            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.length - 1, 1);
+            twoLevelsUp.subtasks.splice(twoLevelsUp.subtasks.findIndex(t => t.name === oneLevelUp.name) + 1, 0, taskObject);
+            List.renderTasks(twoLevelsUp.name ? id.substring(0, id.length - 4) : "root");
+            document.getElementById("header_" + (id.length > 3 ? id.substring(0, id.length - 4) + "_" : "") + (Number(id.substring(id.length - 3, id.length - 2)) + 1)).children[1].focus();
+        }else if(direction === "right" && prevSibling){
+            prevSibling.subtasks.push(taskObject);
+            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name), 1);
+            List.renderTasks(oneLevelUp.name ? id.substring(0, id.length - 2) : "root");
+            document.getElementById( "header_" + (id.length > 1 ? id.substring(0, id.length - 2) + "_" : "") + (prevSibling ? (oneLevelUp.subtasks.findIndex(t => t.name === prevSibling.name)) + "_" : "") + (prevSibling.subtasks.length - 1) ).children[1].focus();
+        }else if(direction === "up" && prevSibling){
+            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name), 1);
+            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === prevSibling.name), 0, taskObject);
+            List.renderTasks(oneLevelUp.name ? id.substring(0, id.length - 2) : "root");
+            document.getElementById("header_" + (id.length > 1 ? id.substring(0, id.length - 2) + "_" : "") + (prevSibling ? oneLevelUp.subtasks.findIndex(t => t.name === prevSibling.name) - 1: "") ).children[1].focus();
+        }else if(direction === "down" && nextSibling){
+            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name), 1);
+            oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === nextSibling.name) + 1, 0, taskObject);
+            List.renderTasks(oneLevelUp.name ? id.substring(0, id.length - 2) : "root");
+            document.getElementById("header_" + (id.length > 1 ? id.substring(0, id.length - 2) + "_" : "") + (nextSibling ? oneLevelUp.subtasks.findIndex(t => t.name === nextSibling.name) + 1: "")).children[1].focus();
+        }
+        taskBeingDragged = false;
+    }
+    
+    /**
+     * Dismiss a task
+     * @param id-The id of the task being dismissed
+     * return null
+    **/
+    static dismissTask(id){
+        const oneLevelUp = List.getTaskFromId(id.substring(0, id.length - 2));
+        const taskObject = List.getTaskFromId(id);
+        oneLevelUp.subtasks.splice(oneLevelUp.subtasks.findIndex(t => t.name === taskObject.name), 1);
+        dismissedTasks.push(taskObject);
+        List.renderTasks(id.substring(0, id.length - 2));
+    }
+    
+    /**
+     * On keyup in a task, save it, or delete it if it's now blank
      * @param name-the current task name according to the HTML
      * @param id-the reference of the task, as an underscore-separated string
      * @param list-the List being edited
      * @return an updated task list
      **/
-    static deleteOrSaveOnKeyup(name, id, list) {
+    static deleteOrSaveOnKeyup(name, id) {
         let taskList;
         let reference;
         if (!name) {
-            this.deleteTask(id, list);
-            return list.tasks;
+            Task.deleteTask(id, list);
+            return tasks;
         }
-        else if(!list.taskBeingDragged){
-            taskList = list.tasks;
+        else if(!taskBeingDragged){
+            taskList = tasks;
             reference = id.split("_").map(Number);
             for (let i = 0; i < reference.length - 1; i++) {
                 const ref = reference[i];
                 taskList = taskList[ref].subtasks;
             }
             taskList[reference[reference.length - 1]].name = name;
-            return list.tasks;
+            return tasks;
         }else{
-            return list.tasks;
+            return tasks;
         }
     }
-    /**
-     * Delete a task
-     * @param reference-the reference of the task, as an underscore-separated string
-     * @param list-the List being edited
-     **/
+    
     static deleteTask(id, list) {
         let reference = id.split("_").map(Number);
         let taskList = list.tasks;

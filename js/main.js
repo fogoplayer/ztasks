90d5084522d90e687bea42902431ab7a4a60e0b4
Zarin Loosli <fogoplayer@gmail.com>
2018-09-15 12:56:39 -0600

    Add firebase

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ window.List = List;
 window.Task = Task;
 window.DismissedList = DismissedList;
 
+//Setup dummy data
 window.tasks = [{
         name: "task1",
         checked: false,
@@ -56,7 +57,34 @@ window.dismissedTasks = [{
         subtasks: []
     }]
 }];
+
 window.taskBeingDragged = false;
+
+//Firebase
+if (!firebase || !firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyBbjO5GBmmydh3nkLqK9t9OxsvH_9OqYeQ",
+        authDomain: "ztasks-1.firebaseapp.com",
+        databaseURL: "https://ztasks-1.firebaseio.com",
+        projectId: "ztasks-1",
+        storageBucket: "ztasks-1.appspot.com",
+        messagingSenderId: "628882993959"
+    });
+    firebase.firestore().settings({
+        timestampsInSnapshots: true
+    });
+    firebase.firestore().enablePersistence()
+        .catch(function(err) {
+            if (err.code === 'failed-precondition') {
+                alert("Warning: You have LDS Youth open in another tab, so your data will not be available while offline in this tab. You will still be able to access site content, but if you would like your content to sync offline please close the app elsewhere.");
+            }
+            else if (err.code === 'unimplemented') {
+                M.toast({
+                    html: "Warning: your browser does not support saving your data offline. Please update your browser if you would like to use LDS Youth without an internet connection"
+                });
+            }
+        });
+}
 
 //Initial render
 if (window.location.pathname === "/") {

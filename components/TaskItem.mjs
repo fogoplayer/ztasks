e0e8734eb43952d8cd86eb34ac4c6b606254d9e0
Zarin Loosli <zarinloosli@gmail.com>
2021-12-21 01:01:00 -0700

    Bind name data to prop

--- components/TaskItem.mjs
+++ components/TaskItem.mjs
@@ -1,59 +1,76 @@
 import "../components/CustomCheckbox.mjs";
 
+/**
+ * @param complete whether the task is completed or not
+ * @param name the name of the task
+ * @param next-reminder the next the the user will be reminded of the task
+ * @param due-date the due date of the task
+ * @param description a description of the task
+ * @param is-recurring whether or not the task is recurring
+ * @param show-subtasks a boolean for whether or not to show the subtasks
+  */
+
+
 class TaskItem extends HTMLElement {
   constructor() {
     super();
 
     // Shadow checkbox and label
     const template = document.createElement("template");
-    template.innerHTML = `<li class="task${this.getAttribute("has-due-date") ? " has-due-date" : ""
-      }${this.getAttribute("has-reminder") ? " has-reminder" : ""
-      }${this.getAttribute("is-recurring") ? " is-recurring" : ""
-      }${this.getAttribute(" has-description") ? "has-description" : ""
+    template.innerHTML = `    <li class="task${this.hasDueDate ? " has-due-date" : ""
+      }${this.hasReminder ? " has-reminder" : ""
+      }${this.isRecurring ? " is-recurring" : ""
+      }${this.hasDescription ? "has-description" : ""
       }"
       >
         <div class="task-preview">
           <span class="material-icons task-drag-handle"> drag_handle </span>
-          <custom-checkbox class="task-check"></custom-checkbox>
-          <input type="text" class="task-name" />
-          <a class="details-link" href="../task-details"
-            ><div class="chip task-data">
-              <span class="material-icons notif-indicator">
-                notifications_active
-              </span>
-              <span class="task-due-date">Tomorrow</span>
-              <span class="material-icons recurring-indicator">cached</span>
-              <span class="material-icons description-indicator">notes</span>
-            </div>
-            <button class="task-more">
-              <i class="material-icons">more_vert</i>
-            </button>
-          </a>
+          <custom-checkbox class="task-check" ${this.complete ? " checked" : ""
+      }></custom-checkbox>
+      <input type="text" class="task-name" value="${this.name}"/>
+      <a class="details-link" href = "../task-details">
+        <div class="chip task-data">
+          <span class="material-icons notif-indicator">
+            notifications_active
+          </span>
+          <span class="task-due-date">Tomorrow</span>
+          <span class="material-icons recurring-indicator">cached</span>
+          <span class="material-icons description-indicator">notes</span>
         </div>
-        <ul class="subtasks"></ul>
-      </li>
-      <link rel="stylesheet" href="/styles/components/TaskItem.css" />
+        <button class="task-more">
+          <i class="material-icons">more_vert</i>
+        </button>
+      </a >
+    </div >
+    <ul class="subtasks"></ul>
+        </li >
+    <link rel="stylesheet" href="/styles/components/TaskItem.css" />
     `;
-
-
-
 
     // Create
     this.attachShadow({ mode: "open" });
-
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    // Data binding
+    this.shadowRoot.querySelector(".task-name").onchange = (e) => {
+      this.setAttribute("name", e.target.value);
+    };
 
   }
 
   // Attributes
   static get observedAttributes() {
-    return ["checked"];
+    return ["complete", "name"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     switch (name) {
-      case "checked":
-        this.checkedChanged(oldValue, newValue);
+      case "complete":
+        this.completeChanged(oldValue, newValue);
+        break;
+
+      case "name":
+        this.parentChangedName(oldValue, newValue);
         break;
 
       default:
@@ -61,34 +78,40 @@ class TaskItem extends HTMLElement {
     }
   }
 
-  // Checked
-  get checked() {
-    return this.hasAttribute("checked");
+  // Complete
+  get complete() {
+    return this.hasAttribute("complete");
   }
 
-  set checked(val) {
+  set complete(val) {
     if (val) {
-      this.setAttribute("checked", "");
+      this.setAttribute("complete", "");
     } else {
-      this.removeAttribute("checked");
+      this.removeAttribute("complete");
     }
   }
 
-  checkedChanged(oldValue, newValue) {
-    console.log("Checked changed:", this.checked, oldValue, newValue);
+  completeChanged(oldValue, newValue) {
 
-    // Displayed checkbox
     if (newValue !== null) {
-      this.shadowRoot.querySelector(".skew-checkbox").classList.add("checked");
+      this.shadowRoot.querySelector("custom-checkbox").checked = true;
     } else {
-      this.shadowRoot
-        .querySelector(".skew-checkbox")
-        .classList.remove("checked");
+      this.shadowRoot.querySelector("custom-checkbox").checked = false;
     }
+  }
 
-    // Hidden Checkbox
-    this.querySelector("input").checked = (newValue !== null);
+  // Name
+  get name() {
+    return this.getAttribute("name");
+  }
+
+  set name(val) {
+    this.setAttribute("name", val);
+  }
+
+  parentChangedName(oldValue, newValue) {
+    this.shadowRoot.querySelector(".task-name").value = this.name;
   }
 }
 
-window.customElements.define("task-item", TaskItem);
+window.customElements.define("task-item", TaskItem);;;

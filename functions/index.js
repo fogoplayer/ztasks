8d642bdb34b0d50922f5eabe739a72d70b95ce4a
Zarin Loosli <zarinloosli@gmail.com>
2021-12-30 17:31:57 -0700

    Add cloud function to retrieve task

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,25 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-exports.getTaskWithSubtasks = functions.https.onRequest((req, res) => {
+exports.getTaskWithSubtasks = functions.https.onRequest(async (req, res) => {
   const path = req.path.split("/");
-  const id = path[path.length - 1]
-  res.send(admin.firestore().collection('tasks').get())
-  // .doc(id)
-  // .then(task => res.send(JSON.stringify(top)));
+  const id = path[path.length - 1];
+  const taskTree = await getTaskTree(id);
+  res.send(taskTree);
 
+  async function getTaskTree(taskId) {
+    const doc = await admin.firestore().collection("tasks").doc(taskId).get();
+    const data = doc.data();
+
+    data.id = doc.id;
+    for (let i = 0; i < data.subtasks.length; ++i) {
+      data.subtasks[i] = await getTaskTree(data.subtasks[i]);
+    }
+
+    return data;
+  }
 });

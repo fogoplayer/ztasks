333fa59009039e98657a75b9276b2da7ce67686b
Zarin Loosli <zarinloosli@gmail.com>
2021-12-31 16:10:08 -0700

    Format

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,9 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
-import { getFirestore, doc, collection, getDoc, addDoc, updateDoc, arrayUnion } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import firebaseConfig from "../firebase/firebase_config.mjs";
+import {
+  getFirestore,
+  doc,
+  collection,
+  getDoc,
+  addDoc,
+  updateDoc,
+  arrayUnion,
+  connectFirestoreEmulator,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import firebaseConfig from "/firebase/firebase_config.mjs";
 import Task from "./TaskSchema.mjs";
 
 // Initialize Firebase
@@ -14,11 +27,14 @@ const auth = getAuth();
 
 // Other vars
 export let user = undefined;
-const usersRef = collection(firestore, "users")
-const tasksRef = collection(firestore, "tasks")
+const usersRef = collection(firestore, "users");
+const tasksRef = collection(firestore, "tasks");
+
+// Emulator
+connectFirestoreEmulator(firestore, "localhost", 8080);
 
 export function getTask(id) {
-  return fetch("/task/" + id).then(res => res.json());
+  return fetch("/task/" + id).then((res) => res.json());
 }
 
 export async function getTaskLists() {
@@ -39,19 +55,26 @@ export function login(email, password) {
 }
 
 export async function createSubtask(id) {
+  const parentRef = await doc(tasksRef, id);
   const subtaskRef = await addDoc(collection(firestore, "tasks"), {
     name: "",
-    checked: false,
+    complete: false,
     description: "",
     recurring: false,
     reminders: [],
     subtasks: [],
     showSubtasks: true,
   });
-  const parentRef = await doc(tasksRef, id);
+
+  // Add ID to subtask
+  await updateDoc(subtaskRef, {
+    id: subtaskRef.id,
+  });
+
+  // Add subtask to parent
   await updateDoc(parentRef, {
-    subtasks: arrayUnion(subtaskRef.id)
+    subtasks: arrayUnion(subtaskRef.id),
   });
 }
 
-export default app
+export default app;

32b8a9b6106e94de49c31fad345308058cac261f
zarinloosli <zarinloosli@gmail.com>
2024-02-16 01:21:19 +0000

    Set up list item grid

--- src/components/ListItem.mjs
+++ src/components/ListItem.mjs
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from "lit";
 import Task from "../models/Task.mjs";
+import globalCss from "../global-styles/global.css.mjs";
 
 export class ListItem extends LitElement {
   /** @type {Task?} */
@@ -15,26 +16,55 @@ export class ListItem extends LitElement {
     this.open = true;
   }
 
-  static styles = css``;
-
   render() {
-    return html`<details ?open="${this.open}">
-      <summary>
-        <button class="toggle-subtasks"></button>
+    return html`<section ?open="${this.open}">
+      <header>
+        <button class="toggle-subtasks" @click=${() => (this.open = !this.open)}>
+          <span class="material-symbols"> chevron_right </span>
+        </button>
         <span class="drag-handle"></span>
         <input type="checkbox" ?checked=${this.task?.complete} />
         <input type="text" class="task-title" value="${this.task?.title}" />
         <a href="" class="details-link"></a>
-      </summary>
-      <ul class="subtasks">
+      </header>
+      <ul class="subtasks unstyled-ul">
         ${this.task?.subtasks.map((subtask) => html`<list-item .task=${subtask}></list-item>`)}
       </ul>
-    </details>`;
+    </section>`;
   }
 
-  updated() {
-    console.log(this.task);
-  }
+  static styles = [
+    globalCss,
+    css`
+      section {
+        display: grid;
+        grid-template-columns: auto auto auto 1fr auto;
+        grid-template-rows: auto auto;
+      }
+
+      header {
+        display: grid;
+        grid-column: 1/-1;
+        grid-template-columns: subgrid;
+
+        list-style: none;
+
+        &::-webkit-section-marker,
+        &::marker {
+          display: none;
+        }
+
+        .toggle-subtasks {
+          cursor: pointer;
+        }
+      }
+
+      .subtasks {
+        display: grid;
+        grid-column: 2/-1;
+      }
+    `,
+  ];
 }
 
 customElements.define("list-item", ListItem);
